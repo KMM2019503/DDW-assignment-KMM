0911e77bd5fdfa650fda4b0dfb8e2307b7961a39
AZ2411 <abizo050@gmail.com>
2024-02-22 10:24:31 +0630

    90% complete

--- js/home.js
+++ js/home.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const nav = document.getElementById("nav");
   const navLinks = document.getElementById("nav-links");
-  const makHeader = document.getElementById("mak-first-header");
-  const makfirstdetail = document.getElementById("mak-first-detail");
-  const makfirstImage = document.getElementById("mak-first-image");
   const modal = document.getElementById("modal");
 
   modal.classList.add("active");
@@ -35,11 +32,56 @@ document.addEventListener("DOMContentLoaded", function () {
   const throttledScroll = throttle(handleScroll, 200);
   window.addEventListener("scroll", throttledScroll);
 });
-
+const heroContent = document.querySelector("[data-hero-content]");
 const handleCloseModal = () => {
   let modal = document.getElementById("modal");
   let modalBg = document.getElementById("modal-bg");
   modal.style.display = "none";
   modalBg.style.display = "none";
   document.body.classList.remove("over-flow-hidden");
+  heroContent.classList.add("active");
+  autoChangeSlide();
 };
+// slide change function
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const carousel = document.querySelector("[data-carousel]");
+const slides = carousel.querySelector("[data-slides]");
+
+function showNextSlide() {
+  const activeSlide = slides.querySelector("[data-active]");
+  let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+
+  if (newIndex >= slides.children.length) {
+    newIndex = 0;
+  }
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
+
+function autoChangeSlide() {
+  setInterval(() => {
+    showNextSlide();
+  }, 5000);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+    if (newIndex < 0) {
+      newIndex = slides.children.length - 1;
+    }
+
+    if (newIndex >= slides.children.length) {
+      newIndex = 0;
+    }
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
+
+// Start automatic slide change after the page loads
