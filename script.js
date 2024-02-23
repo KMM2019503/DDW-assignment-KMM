7d10813f77e983c6a06c466dbc141e871912e749
AZ2411 <abizo050@gmail.com>
2024-02-23 11:08:44 +0630

    dorp down feature 80% finished

--- script.js
+++ script.js
@@ -63,3 +63,22 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
+
+//drop down menu
+function toggleDropdown() {
+  var dropdownMenu = document.getElementById("myDropdown");
+  dropdownMenu.classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
